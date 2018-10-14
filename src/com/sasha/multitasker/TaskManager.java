52fcebd802c6b23850eb507d8c412dc30305102c
Sasha <sashwho@vivaldi.net>
2018-10-14 15:13:31 -0700

    queueTask() functions should notify the lock.

--- src/com/sasha/multitasker/TaskManager.java
+++ src/com/sasha/multitasker/TaskManager.java
@@ -16,7 +16,8 @@ public class TaskManager {
 
     /**
      * Add a Task to the queue to be executed.
-     * @param task The task to run
+     *
+     * @param task  The task to run
      * @param async Whether to run it on the thread that TaskManager#tick() is being called on, or its own thread.
      */
     public void queueTask(Task task, boolean async) {
@@ -26,7 +27,27 @@ public class TaskManager {
             taskQueue.add(task);
         } finally {
             lock.unlock();
+            runLock.notify();
         }
+    }
+
+    public void queueTask(Task task, boolean async, long delay) {
+        task.async = async;
+        new Thread(() -> {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            lock.lock();
+            try {
+                taskQueue.add(task);
+            } finally {
+                lock.unlock();
+                runLock.notify();
+            }
+        }).start();
+
     }
 
     /**
@@ -35,33 +56,37 @@ public class TaskManager {
      */
     public void tick() {
         Task someTask;
-        synchronized (runLock){
-            while(true) {
-                if (taskQueue.isEmpty()) {
-                    try {
-                        runLock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        synchronized (runLock) {
+            while (true) {
+                try {
+                    if (taskQueue.isEmpty()) {
+                        try {
+                            runLock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        continue;
                     }
-                    continue;
-                }
-                someTask = taskQueue.get(0);
-                if (someTask.async) {
-                    new Thread(someTask).start();
+                    someTask = taskQueue.get(0);
+                    if (someTask.async) {
+                        new Thread(someTask).start();
+                        lock.lock();
+                        try {
+                            taskQueue.remove(someTask);
+                        } finally {
+                            lock.unlock();
+                        }
+                        continue;
+                    }
+                    someTask.run();
                     lock.lock();
                     try {
                         taskQueue.remove(someTask);
                     } finally {
                         lock.unlock();
                     }
-                    continue;
-                }
-                someTask.run();
-                lock.lock();
-                try {
-                    taskQueue.remove(someTask);
-                } finally {
-                    lock.unlock();
+                } catch (Exception e) {
+                    //
                 }
             }
         }
